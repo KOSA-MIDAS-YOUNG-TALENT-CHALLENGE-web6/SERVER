d734ab17267e319539bcc2e4d7d1384b35032a4d
jaemin <sonjm3518@dsm.hs.kr>
2022-11-03 17:00:58 +0900

    feat: 시간 등록

---
+++ src/main/kotlin/com/example/server/domain/user/service/ModifyTodayOfficeGoingTimeService.kt
@@ -0,0 +1,19 @@
+package com.example.server.domain.user.service
+
+import com.example.server.domain.user.facade.UserFacade
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import java.time.LocalDateTime
+
+@Service
+class ModifyTodayOfficeGoingTimeService(
+    private val userFacade: UserFacade
+) {
+
+    @Transactional
+    fun execute() {
+        val user = userFacade.getCurrentUser()
+
+        user.modifyTodayOfficeGoingTime(LocalDateTime.now())
+    }
+}
