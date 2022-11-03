d4bc76e80ad2a84dbfcdd0e9012b462b110a6185
jaemin <sonjm3518@dsm.hs.kr>
2022-11-03 17:00:41 +0900

    feat: 사원증 등록

---
+++ src/main/kotlin/com/example/server/domain/user/presentation/dto/request/VerifyUserRequest.kt
@@ -0,0 +1,5 @@
+package com.example.server.domain.user.presentation.dto.request
+
+data class VerifyUserRequest(
+    val image: String
+)
--- src/main/kotlin/com/example/server/domain/user/service/VerifyUserService.kt
+++ src/main/kotlin/com/example/server/domain/user/service/VerifyUserService.kt
@@ -1,6 +1,7 @@
 package com.example.server.domain.user.service
 
 import com.example.server.domain.user.facade.UserFacade
+import com.example.server.domain.user.presentation.dto.request.VerifyUserRequest
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -10,9 +11,9 @@ class VerifyUserService(
 ) {
 
     @Transactional
-    fun execute(image: String) {
+    fun execute(request: VerifyUserRequest) {
         val user = userFacade.getCurrentUser()
 
-        user.verifyUser(true, image)
+        user.verifyUser(true, request.image)
     }
 }
