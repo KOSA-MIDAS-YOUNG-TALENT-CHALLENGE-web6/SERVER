bf1da6b62972eacb06dff20f1ec13a5640f92e9c
jaemin <sonjm3518@dsm.hs.kr>
2022-11-03 14:24:58 +0900

    feat: config 설정

---
+++ server/src/main/kotlin/com/example/server/global/config/PropertiesConfig.kt
@@ -0,0 +1,9 @@
+package com.example.server.global.config
+
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
+import org.springframework.context.annotation.Configuration
+
+@ConfigurationPropertiesScan("com.example.server")
+@Configuration
+class PropertiesConfig {
+}
