3ff0ed987baff863d191240fb0a38c93585a8251
jaemin <sonjm3518@dsm.hs.kr>
2022-11-04 00:04:48 +0900

    feat: Feed

---
+++ src/main/kotlin/com/example/server/domain/feed/domain/Feed.kt
@@ -0,0 +1,38 @@
+package com.example.server.domain.feed.domain
+
+import com.example.server.domain.user.domain.User
+import com.example.server.global.entity.BaseEntity
+import javax.persistence.Entity
+import javax.persistence.FetchType
+import javax.persistence.JoinColumn
+import javax.persistence.ManyToOne
+import javax.persistence.Table
+import javax.validation.constraints.NotNull
+
+@Entity
+@Table(name = "tbl_user")
+class Feed(
+
+    @field:NotNull
+    val content: String,
+
+    isDone: Boolean,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false, columnDefinition = "BINARY(16)")
+    val user: User,
+
+    val isAdmin: Boolean
+
+) : BaseEntity() {
+    var isDone = isDone
+        protected set
+
+    fun modifyIsDone(isDone: Boolean) {
+        this.isDone = isDone
+    }
+
+    fun getIsAdmin(): Boolean {
+        return this.isAdmin
+    }
+}
