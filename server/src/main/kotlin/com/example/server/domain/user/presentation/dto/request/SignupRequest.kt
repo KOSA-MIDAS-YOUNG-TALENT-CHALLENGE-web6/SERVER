e52861c68511e7ea1f2002b92c8d8a9db1593742
jaemin <sonjm3518@dsm.hs.kr>
2022-11-03 14:24:48 +0900

    feat: Signup

---
+++ server/src/main/kotlin/com/example/server/domain/user/presentation/dto/request/SignupRequest.kt
@@ -0,0 +1,22 @@
+package com.example.server.domain.user.presentation.dto.request
+
+import javax.validation.constraints.Email
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.Pattern
+
+data class SignupRequest(
+
+    @field: Email
+    @field: NotBlank(message = "email은 필수 입력입니다.")
+    val email: String,
+
+    val name: String,
+
+    @field:Pattern(
+        regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}$",
+        message = "password는 영문자와 숫자, 특수문자를 모두 포함해서 8~16자리 이내로 입력해주세요."
+    )
+    val password: String,
+
+    val rePassword: String
+)
