5c193296a12168ff036253677f832eb1ab728c0b
jaemin <sonjm3518@dsm.hs.kr>
2022-11-04 02:59:44 +0900

    test

--- src/main/kotlin/com/example/server/domain/image/presentation/ImageController.kt
+++ src/main/kotlin/com/example/server/domain/image/presentation/ImageController.kt
@@ -6,11 +6,10 @@ import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.multipart.MultipartFile
-import javax.validation.Valid
 
 @Tag(name = "이미지", description = "이미지 관련 API입니다.")
 @RestController
@@ -21,7 +20,7 @@ class ImageController(
     @Operation(summary = "이미지 업로드")
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/images")
-    fun imageUpload(@Valid @RequestBody image: MultipartFile): ImageResponse {
+    fun imageUpload(@RequestParam image: MultipartFile): ImageResponse {
         return imageService.imageUpload(image)
     }
 }
